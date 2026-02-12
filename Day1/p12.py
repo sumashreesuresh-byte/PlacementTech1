d430656cf5065ec44b6053d9cea9622681cf54ea
Summ777 <239852203+sumashreesuresh-byte@users.noreply.github.com>
2026-02-12 10:40:05 +0530

    Merged remote changes

---
+++ Day1/p12.py
@@ -0,0 +1,24 @@
+n = int(input("Enter the number of elements in the array\n"))
+
+arr = []
+
+print("Enter the elements in the array")
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+
+freq = {}
+
+for num in arr:
+    freq[num] = freq.get(num, 0) + 1
+
+max_freq = max(freq.values())
+
+if max_freq == 1:
+    print("The mode of the array is none")
+else:
+    for key in freq:
+        if freq[key] == max_freq:
+            print("The mode of the array is")
+            print(key)
+            break
