6cb360257716a104e54813dab3dbb83ae22098b8
Summ777 <239852203+sumashreesuresh-byte@users.noreply.github.com>
2026-02-12 10:40:48 +0530

    Updated Prob4

--- Day1/Prob4.py
+++ Day1/Prob4.py
@@ -2,7 +2,7 @@
 v =int(input("Enter the number of vehicles (v): "))
 w=int(input("Enter the number of wheels (w): "))
 # Check for invalid input conditions
-if w %2!=0 or w<2*vorw>4*v:
+if w %2!=0 or w<2*v:
     print("INVALID INPUT")
 else:
     fw = (w-(2 * v)) // 2
